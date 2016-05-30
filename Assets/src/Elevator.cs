ebd75d6c6938dfc771fdb77a6ad10a93b1df6af4
David DeSimone <lord.good.mail@gmail.com>
2016-05-29 21:15:07 -0700

    Work in progress commit for making elevator move on a parameterized path.

--- Assets/src/Elevator.cs
+++ Assets/src/Elevator.cs
@@ -7,6 +7,9 @@ public class Elevator : MonoBehaviour {
 	public GameObject ActivationPanel;
 	public float Acceleration;
 	public Vector3 Direction;
+	public float MaximumDistance;
+	public float DistanceUntilBreak;
+	public bool DebugDraw;
 
 	private GameObject playerCharacter;
 	private ConstantForce cforce;
@@ -14,34 +17,60 @@ public class Elevator : MonoBehaviour {
 	private enum MOTION_STATE
 	{
 		AT_REST,
-		POSITIVE,
-		NEGATIVE
+		IN_MOITION
 	}
 
 	private MOTION_STATE currentMotionState;
+
+	private Vector3 forceStep;
+	private const int forceInterationMaximum = 1024;
+	private Vector3 startPosition;
+	private Vector3 endPosition;
+	private int iterationCount = 0;
 
 	// Use this for initialization
 	void Start () {
 		playerCharacter = GameObject.Find ("Character");
 		cforce = gameObject.GetComponent<ConstantForce> ();
 		currentMotionState = MOTION_STATE.AT_REST;
+		startPosition = gameObject.transform.position;
+		endPosition = -((Direction * MaximumDistance) + startPosition); 
+
 	}
 
 	void FixedUpdate () {
+		if (DebugDraw) {
+			Debug.DrawRay (startPosition, endPosition, Color.blue);
+		}
+
 		if (Input.GetKey (ActivationKeyCode))
 		{
 			Vector3 forwardRay = playerCharacter.transform.forward * 10.0f;
 			RaycastHit hit = new RaycastHit ();
 			if (Physics.Raycast (playerCharacter.transform.position, forwardRay, out hit, 20.0f)) {
 				if (hit.collider.name == ActivationPanel.name && !IsInMotion()) {
-					cforce.force = Direction * gameObject.GetComponent<Rigidbody>().mass * Acceleration;
-					currentMotionState = MOTION_STATE.POSITIVE;
+					cforce.relativeForce = Direction * gameObject.GetComponent<Rigidbody>().mass * Acceleration;
+					forceStep = cforce.relativeForce / forceInterationMaximum;
+					currentMotionState = MOTION_STATE.IN_MOITION;
+				}
+			}
+		}
+
+		if (IsInMotion()) {
+			float distanceToEnd = Vector3.Distance (gameObject.transform.position, endPosition);
+			if (distanceToEnd <= DistanceUntilBreak) {
+				cforce.relativeForce -= forceStep;
+				iterationCount++;
+
+				if (iterationCount == forceInterationMaximum) {
+					currentMotionState = MOTION_STATE.AT_REST;
+					iterationCount = 0;
 				}
 			}
 		}
 	}
 
-	private bool IsInMotion() {
+	public bool IsInMotion() {
 		return currentMotionState != MOTION_STATE.AT_REST;
 	}
 }
---
+++ Assets/src/TrackElevator.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackElevator : MonoBehaviour {
+
+	public Vector3[] MotionPath;
+	public float[] TargetVelocity;
+	public bool DebugDraw;
+
+	private enum MOTION_STATE
+	{
+		IN_MOTION,
+		AT_REST
+	}
+
+	private Vector3[] originPath;
+	private MOTION_STATE currentMotionState;
+	private int currentTargetPoint = 0;
+	private ConstantForce constForce;
+	private Vector3 debugDirection;
+
+	private const float MIN_DIST = 3.0f;
+
+	private const float distanceDivisor = 3;
+	private float totalDistanceUntilNextPoint;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Assert (MotionPath.Length + 1 == TargetVelocity.Length);
+		currentMotionState = MOTION_STATE.AT_REST;
+		originPath = new Vector3[MotionPath.Length + 1];
+		originPath [0] = gameObject.transform.position;
+		for (int i = 0; i < MotionPath.Length; ++i) {
+			originPath [i + 1] = MotionPath [i];
+		}
+
+		constForce = gameObject.GetComponent<ConstantForce> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (DebugDraw) {
+			for (int i = 1; i < originPath.Length; ++i) {
+				Debug.DrawRay (originPath [i - 1], originPath [i], Color.blue);
+			}
+		}
+
+		if (Input.GetKeyUp (KeyCode.F)) {
+			currentTargetPoint = 1;
+			currentMotionState = MOTION_STATE.IN_MOTION;
+			moveTowardsPoint (originPath [currentTargetPoint]);
+			Debug.Log ("Activating Elevator based on player action...");
+		}
+
+		if (currentMotionState == MOTION_STATE.IN_MOTION
+			&& Vector3.Distance(gameObject.transform.position, originPath[currentTargetPoint]) < MIN_DIST) {
+
+			Debug.Log ("Elevator is within proximity of destination point, relocating...");
+			currentTargetPoint++;
+			if (currentTargetPoint > originPath.Length) {
+				currentTargetPoint = 0;
+				currentMotionState = MOTION_STATE.AT_REST;
+				constForce.force = Vector3.zero;
+			} else {
+				Debug.Log ("Moving to next target...");
+				moveTowardsPoint (originPath [currentTargetPoint]);
+			}
+		}
+
+		if (currentMotionState == MOTION_STATE.IN_MOTION) {
+			float distanceUntilNextPoint = Vector3.Distance(gameObject.transform.position, originPath[currentTargetPoint]);
+			Debug.Log (string.Format ("Logging distance until next target {0}", distanceUntilNextPoint));
+		}
+			
+		if (DebugDraw) {
+			Debug.DrawRay (gameObject.transform.position, debugDirection, Color.red);
+		}
+
+
+	}
+
+	private void moveTowardsPoint(Vector3 p) {
+		Vector3 diff = p - gameObject.transform.position;
+		gameObject.GetComponent<Rigidbody>().AddForce(diff * gameObject.GetComponent<Rigidbody>().mass * TargetVelocity[currentTargetPoint]);
+		if (DebugDraw) {
+			debugDirection = diff;
+		}
+	}
+}
