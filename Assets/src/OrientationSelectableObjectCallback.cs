c3893e49cce04c4fd5559193e271765bd3ecc7fe
David DeSimone <lord.good.mail@gmail.com>
2016-06-05 23:51:22 -0700

    Fixing up rotateAt code

--- Assets/src/OrientationSelectableObjectCallback.cs
+++ Assets/src/OrientationSelectableObjectCallback.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class OrientationSelectableObjectCallback : SelectableObjectCallback {
 
     public GameObject OrentationReference;
+    public bool DebugDraw;
     private bool rotatingTowards = false;
+    private bool movingTowards = false;
     private Quaternion rotateTowards;
 
 	// Use this for initialization
@@ -15,8 +17,14 @@ public class OrientationSelectableObjectCallback : SelectableObjectCallback {
 	// Update is called once per frame
 	void Update ()
     {
+        if (DebugDraw)
+        {
+            Debug.DrawRay(gameObject.transform.position, gameObject.transform.forward * 7.0f, Color.magenta);
+        }
+
         if (rotatingTowards)
         {
+            rotateTowards = Quaternion.LookRotation(OrentationReference.transform.position - gameObject.transform.position, OrentationReference.transform.up);
             gameObject.transform.rotation = Quaternion.Slerp(gameObject.transform.rotation, rotateTowards, Time.deltaTime);
         }
         
@@ -25,7 +33,7 @@ public class OrientationSelectableObjectCallback : SelectableObjectCallback {
     override public bool ObjectSelected()
     {
         rotatingTowards = true;
-        rotateTowards = Quaternion.LookRotation(OrentationReference.transform.position);
+        movingTowards = true;
         Debug.Log("Second Selected...");
         return true;
     }
